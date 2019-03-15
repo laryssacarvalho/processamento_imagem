afe87671789c618784bfe9a2552add6f4193413d
Laryssa Carvalho <laryssaalves_carvalho@hotmail.com>
2019-03-14 23:16:41 -0300

    Initial commit

---
+++ aula03-1.py
@@ -0,0 +1,8 @@
+import cv2
+img = cv2.imread('dori.jpg')
+cv2.imshow('Img', img)
+print('Shape: ', img.shape)
+print('Size: ' , img.size)
+print('Type: ' , img.dtype)
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ slides.py
@@ -0,0 +1,49 @@
+import glob
+import cv2
+
+def transition(img1, img2):
+    for IN in range(0,5):
+        fadein = IN/float(5)
+        dst = cv2.addWeighted(img1, 1-fadein, img2, fadein, 0)
+        cv2.imshow('Img', dst)
+        key = cv2.waitKey(100)
+        if key == 113:
+            exit()
+
+def watermark(img,file):
+        overlay = img.copy()
+        output = img.copy()
+        height, width = img.shape[:2]
+        alpha = 0.3
+        namestamp = "Laryssa"
+        cv2.putText(overlay, namestamp.format(alpha), (width-150, height-30), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), thickness=2)
+        cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+        cv2.imwrite("watermarks{}".format(file), output)
+
+def format_images():
+    for file in glob.glob('images/*.jpg'):            
+        img = cv2.imread(file)
+        img = cv2.resize(img,(300,300))
+        img = cv2.copyMakeBorder(img, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=BLACK)            
+        watermark(img,file)        
+            
+key = None
+bordersize = 20
+BLACK = [0,0,0]
+previous_img = None
+
+while True:
+    format_images()
+    for file in glob.glob('watermarksimages/*.jpg'):    
+        if key != 113:
+            img = cv2.imread(file)
+            if previous_img is not None:
+                transition(previous_img, img)
+            cv2.imshow('Img', img)
+            key = cv2.waitKey(2000)
+            previous_img = img
+        else:
+            exit()
+    if key == 113:
+        exit()
+cv2.destroyAllWindows()
