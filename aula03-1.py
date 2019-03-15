035ec4c723fa10a32d2e8d37d91110ab37bbcc6a
Laryssa Carvalho <laryssaalves_carvalho@hotmail.com>
2019-03-14 23:19:03 -0300

    Remoção dos arquivos de aula

--- aula03-1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-img = cv2.imread('dori.jpg')
-cv2.imshow('Img', img)
-print('Shape: ', img.shape)
-print('Size: ' , img.size)
-print('Type: ' , img.dtype)
-cv2.waitKey()
-cv2.destroyAllWindows()
