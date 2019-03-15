d856002d9063606e8c0ae751dcf8ca7a45991d47
Laryssa Carvalho <laryssaalves_carvalho@hotmail.com>
2019-03-14 23:30:47 -0300

    Limpa a pasta de marca dagua antes de criar as imagens

--- slides.py
+++ slides.py
@@ -1,5 +1,7 @@
 import glob
 import cv2
+import shutil
+import os
 
 def transition(img1, img2):
     for IN in range(0,5):
@@ -18,23 +20,29 @@ def watermark(img,file):
         namestamp = "Laryssa"
         cv2.putText(overlay, namestamp.format(alpha), (width-150, height-30), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), thickness=2)
         cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
-        cv2.imwrite("watermarks{}".format(file), output)
+        cv2.imwrite("watermark{}".format(file), output)
 
 def format_images():
     for file in glob.glob('images/*.jpg'):            
         img = cv2.imread(file)
         img = cv2.resize(img,(300,300))
         img = cv2.copyMakeBorder(img, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=BLACK)            
-        watermark(img,file)        
-            
+        watermark(img,file)
+
+def clean_folder():
+    files = glob.glob('/watermarkimages/*')
+    for f in files:
+        os.remove(f)
+        
 key = None
 bordersize = 20
 BLACK = [0,0,0]
 previous_img = None
+clean_folder()
 
 while True:
     format_images()
-    for file in glob.glob('watermarksimages/*.jpg'):    
+    for file in glob.glob('watermarkimages/*.jpg'):    
         if key != 113:
             img = cv2.imread(file)
             if previous_img is not None:
