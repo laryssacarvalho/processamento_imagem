0a29055df7c3b3250c2ab3fb24c267338b2dce00
Laryssa Carvalho <laryssaalves_carvalho@hotmail.com>
2019-04-06 14:43:22 -0300

    Recortar os dados

--- dados.py
+++ dados.py
@@ -1,12 +1,13 @@
 import cv2 
+import numpy as np
 from matplotlib import pyplot as plt
+import glob
 
 img = cv2.imread('dados.jpg',cv2.IMREAD_GRAYSCALE)
 
 ret,thresh = cv2.threshold(img,245,255,cv2.THRESH_BINARY_INV)
 
 medianFiltered = cv2.medianBlur(thresh,5)
-cv2.imshow("Median Filtered Image", medianFiltered)
 
 contours, hierarchy = cv2.findContours(medianFiltered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -16,14 +17,27 @@ for contour in contours:
     if area > 100:
         contour_list.append(contour)
 
-cv2.drawContours(img, contour_list, -1, (0,0,0), 2)
+cv2.drawContours(img, contour_list, -1, (255,255,255), 2)
 cv2.imshow('Objects detected', img)
 
+idx = 0
 for contour in contour_list:
+    idx += 1
     x,y,w,h = cv2.boundingRect(contour)
-    rect = img[y:y+h, x:x+w]
+    roi=img[y:y+h,x:x+w]
+    cv2.imwrite('dados_recortados/'+ str(idx) + '.jpg', roi)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(200,0,0),2)
+cv2.imshow('img',img)
 
-print(rect)
+i = 0
+for file in glob.glob('dados_recortados/*.jpg'):
+	img_dado = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+	ret,thresh_dado = cv2.threshold(img_dado,127,255,cv2.THRESH_BINARY_INV)
+	cv2.imshow("Dado " + str(i), thresh_dado)
+	i+=1
+
+
+# print(rect)
 
 # Setup SimpleBlobDetector parameters.
 # params = cv2.SimpleBlobDetector_Params()
