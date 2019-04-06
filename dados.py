c52491e2a4f5d7448da2294cedc65226e9511a0e
Daniel Braga <dan.b.v.r05@gmail.com>
2019-04-06 16:49:56 -0300

    Escreve na imagem o valor de cada dado, em preto e branco ainda

--- dados.py
+++ dados.py
@@ -1,15 +1,26 @@
-import cv2 
+import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import glob
 
-img = cv2.imread('dados.jpg',cv2.IMREAD_GRAYSCALE)
+#dado uma imagem de um dado, retorna o valor dele
+def valorDado(img):
+    detector = cv2.SimpleBlobDetector_create()
+    keypoints = detector.detect(img)
+    return len(keypoints)
 
-ret,thresh = cv2.threshold(img,245,255,cv2.THRESH_BINARY_INV)
 
-medianFiltered = cv2.medianBlur(thresh,5)
+font = cv2.FONT_HERSHEY_SIMPLEX
+valores = [] 
 
-contours, hierarchy = cv2.findContours(medianFiltered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+img = cv2.imread('dados.jpg', cv2.IMREAD_GRAYSCALE)
+
+ret, thresh = cv2.threshold(img, 245, 255, cv2.THRESH_BINARY_INV)
+
+medianFiltered = cv2.medianBlur(thresh, 5)
+
+contours, hierarchy = cv2.findContours(
+    medianFiltered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 contour_list = []
 for contour in contours:
@@ -17,63 +28,26 @@ for contour in contours:
     if area > 100:
         contour_list.append(contour)
 
-cv2.drawContours(img, contour_list, -1, (255,255,255), 2)
-cv2.imshow('Objects detected', img)
+cv2.drawContours(img, contour_list, -1, (255, 255, 255), 2)
+#cv2.imshow('Objects detected', img)
 
-idx = 0
+idx=0
 for contour in contour_list:
     idx += 1
-    x,y,w,h = cv2.boundingRect(contour)
-    roi=img[y:y+h,x:x+w]
-    cv2.imwrite('dados_recortados/'+ str(idx) + '.jpg', roi)
-    cv2.rectangle(img,(x,y),(x+w,y+h),(200,0,0),2)
-cv2.imshow('img',img)
-
-i = 0
-for file in glob.glob('dados_recortados/*.jpg'):
-	img_dado = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-	ret,thresh_dado = cv2.threshold(img_dado,127,255,cv2.THRESH_BINARY_INV)
-	cv2.imshow("Dado " + str(i), thresh_dado)
-	i+=1
-
-
-# print(rect)
-
-# Setup SimpleBlobDetector parameters.
-# params = cv2.SimpleBlobDetector_Params()
-
-# params.blobColor = 255
-# params.filterByColor = True 
-
-# # Change thresholds
-# # params.minThreshold = 10
-# # params.maxThreshold = 200
-
-# # Filter by Circularity
-# params.filterByCircularity = True
-# params.minCircularity = 0.785
-# params.maxCircularity = 0.785
-
-# ver = (cv2.__version__).split('.')
-# if int(ver[0]) < 3 :
-#     detector = cv2.SimpleBlobDetector(params)
-# else : 
-#     detector = cv2.SimpleBlobDetector_create(params)
-
-# Set up the detector with default parameters.
-# detector = cv2.SimpleBlobDetector_create()
- 
-# keypoints = detector.detect(thresh)
-
-# print("Blobs = ", len(keypoints))
-# for marker in keypoints:
-#     #center
-#     x,y = np.int(marker.pt[0]),np.int(marker.pt[1])
-#     pos = np.int(marker.size / 2)
-#     cv2.rectangle(thresh2,(x-pos,y-pos),(x+pos,y+pos),(255,0,0),1)
+    x, y, w, h = cv2.boundingRect(contour)
+    roi = img[y:y+h, x:x+w]
     
-# cv2.imshow("Blobs = "+ str(len(keypoints)), thresh)
-# cv2.imshow("Blobs",thresh)
+    #escreve o valor retornados da função valorDado na imagem
+    cv2.putText(img, str(valorDado(roi)), (x+w, y+h), font, 1, (0,0,255), thickness=3)    
+    
+    #desenha retangulo
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
+#exibe a imagem com os valores
+cv2.imshow('img', img)
+
+#tentanto exibir colorido
+img_cor = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+cv2.imshow('img_cor', img_cor)
 
 cv2.waitKey(0)
